36154a8efe4c56960ad0ca33634f544acd174333
shenpeihong <shenpeihong@do1.com.cn>
2021-02-20 15:36:58 +0800

    cloud2020

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Shen Peihong on 2021/2/20
+ * Description:
+ *
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] agrs){
+        SpringApplication.run(OrderMain80.class, agrs);
+    }
+
+}
---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud.entities;
+
+/**
+ * Created by Shen Peihong on 2021/2/20
+ * Description:
+ *
+ * @since 1.0.0
+ */
+public class CommonResult {
+}
